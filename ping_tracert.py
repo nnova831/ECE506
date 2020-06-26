2f71bce3826145cdcfed64368d34affbc9c258a7
Nicholas Olgado <nicholas831@gmail.com>
2020-06-26 11:21:20 -0400

    Day 2 Logging

--- ping_tracert.py
+++ ping_tracert.py
@@ -7,9 +7,3 @@ import os
 
 hostname = "google.com" #example
 response = os.system("ping -c 1 " + hostname)
-
-#and then check the response...
-if response == 0:
-  print(hostname, 'is up!')
-else:
-  print(hostname, 'is down!')
