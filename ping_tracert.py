295fb17d994c144fdf5e8aa9d728e2a405b53ace
Nicholas Olgado <nicholas831@gmail.com>
2020-06-27 10:08:23 -0400

    Delete ping_tracert.py

--- ping_tracert.py
+++
@@ -1,9 +0,0 @@
-import platform    # For getting the operating system name
-import subprocess  # For executing a shell command
-import numpy as np
-import csv
-import os
-
-
-hostname = "google.com" #example
-response = os.system("ping -c 1 " + hostname)
